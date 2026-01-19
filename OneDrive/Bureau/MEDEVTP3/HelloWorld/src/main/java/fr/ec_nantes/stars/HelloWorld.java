b8f61e37b44874548c55f94b7d3ed84ac1d5cb31
Bz Safae <bouzidisafa84@gmail.com>
2026-01-19 14:51:57 +0100

    Correspondant

---
+++ OneDrive/Bureau/MEDEVTP3/HelloWorld/src/main/java/fr/ec_nantes/stars/HelloWorld.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package fr.ec_nantes.stars;
+
+/**
+ *
+ * @author dodi
+ */
+
+        // TODO: améliorer la gestion des erreurs
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
